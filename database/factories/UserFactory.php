cef27f476fb513423a5cbaf3029246d30ba80ee3
liwenlu88 <2816842036@qq.com>
2024-10-25 09:03:20 +0800

    生成User模拟数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\PositionStatus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -25,10 +26,13 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'contact_tel' => fake()->phoneNumber(),
+            'account' => Str::random(10),
+            'password' => static::$password ?? static::$password = Hash::make('password'),
             'remember_token' => Str::random(10),
+            'position_status_id' => PositionStatus::inRandomOrder()->first()->id,
+            'description' => fake()->text(200),
+            'car_id' => null,
         ];
     }
 
@@ -37,7 +41,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
