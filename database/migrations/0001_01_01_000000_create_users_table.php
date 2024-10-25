eb4f1f6520659dd59ef3c2111fa67f3f3f06d67b
liwenlu88 <2816842036@qq.com>
2024-10-25 08:51:39 +0800

    调整 users 迁移

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,28 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->comment('用户表');
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('name')->comment('用户名');
+            $table->string('contact_tel', 20)->comment('联系电话');
+            $table->string('account', 20)->comment('账号');
+            $table->string('password')->comment('密码');
             $table->rememberToken();
+            $table->unsignedBigInteger('position_status_id')->nullable()->comment('职位状态');
+            $table->string('description')->nullable()->comment('描述');
+            $table->bigInteger('car_id')->nullable()->comment('车辆ID');
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->softDeletes();
         });
     }
 
@@ -43,7 +33,5 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
     }
 };
